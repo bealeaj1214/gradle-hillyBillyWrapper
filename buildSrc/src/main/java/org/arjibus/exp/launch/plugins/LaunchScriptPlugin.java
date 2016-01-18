c9f2f631a3557e0af4f17ad6f9751bbefcb4a5b7
Alan Beale <bealea@execpc.com>
2016-01-18 12:53:55 -0600

    fix up outputDirectory

--- buildSrc/src/main/java/org/arjibus/exp/launch/plugins/LaunchScriptPlugin.java
+++ buildSrc/src/main/java/org/arjibus/exp/launch/plugins/LaunchScriptPlugin.java
@@ -18,11 +18,15 @@ import org.gradle.platform.base.ComponentBinaries;
 import org.gradle.platform.base.ComponentType;
 import org.gradle.platform.base.ComponentTypeBuilder;
 
+import org.gradle.model.Defaults;
 import org.gradle.model.Model;
 import org.gradle.model.ModelMap;
 import org.gradle.model.Mutate;
 import org.gradle.model.Path;
 import org.gradle.model.RuleSource;
+
+import java.io.File;
+
 
 public class LaunchScriptPlugin implements Plugin<Project>{
 
@@ -44,14 +48,17 @@ public class LaunchScriptPlugin implements Plugin<Project>{
 
 	@ComponentBinaries
 	void createBinaries(ModelMap<LaunchScriptBinarySpec> binaries,
-			    final LaunchScriptSpec  component){
+			    final LaunchScriptSpec  component,
+			    final @Path("buildDir") File buildDir){
 
 	    binaries.create(component.getName(),
 			    new Action<LaunchScriptBinarySpec>() {
 
+				@Override
 				public void execute(LaunchScriptBinarySpec binary){
 				    binary.setMainClass(component.getMainClass());
 				    //binary
+				    binary.setOutputDirectory(new File(buildDir,"launchScripts"));
 				}
 
 			    }
